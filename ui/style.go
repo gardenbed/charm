3dce2d71ba6bb73a64f340d80063ad7b264af271
Milad <moorara@users.noreply.github.com>
2025-02-17 16:02:15 -0500

    Support 256-Color and True colors (#38)

--- ui/style.go
+++ ui/style.go
@@ -43,8 +43,15 @@ const (
 	BgWhite
 )
 
+const (
+	Fg256 ANSICode = 38
+	Bg256 ANSICode = 48
+)
+
+// Style allows multiple ANSI codes.
 type Style []ANSICode
 
+// ANSI Colors
 var (
 	Black   = Style{FgBlack}
 	Red     = Style{FgRed}
@@ -55,6 +62,46 @@ var (
 	Cyan    = Style{FgCyan}
 	White   = Style{FgWhite}
 )
+
+// Fg256Color creates a style for a 256-color foreground color.
+func Fg256Color(code int) Style {
+	if code < 0 || 255 < code {
+		// Default to black if out of range
+		code = 0
+	}
+
+	return Style{Fg256, ANSICode(5), ANSICode(code)}
+}
+
+// Bg256Color creates a style for a 256-color background color.
+func Bg256Color(code int) Style {
+	if code < 0 || 255 < code {
+		// Default to white if out of range
+		code = 7
+	}
+
+	return Style{Bg256, ANSICode(5), ANSICode(code)}
+}
+
+// FgTrueColor creates a style for a 24-bit foreground color.
+func FgTrueColor(r, g, b int) Style {
+	if r < 0 || 255 < r || g < 0 || 255 < g || b < 0 || 255 < b {
+		// Default to black if out of range
+		r, g, b = 0, 0, 0
+	}
+
+	return Style{Fg256, ANSICode(2), ANSICode(r), ANSICode(g), ANSICode(b)}
+}
+
+// BgTrueColor creates a style for a 24-bit background color.
+func BgTrueColor(r, g, b int) Style {
+	if r < 0 || 255 < r || g < 0 || 255 < g || b < 0 || 255 < b {
+		// Default to white if out of range
+		r, g, b = 255, 255, 255
+	}
+
+	return Style{Bg256, ANSICode(2), ANSICode(r), ANSICode(g), ANSICode(b)}
+}
 
 func (s Style) sprintf(format string, a ...interface{}) string {
 	const escape = "\x1b"
